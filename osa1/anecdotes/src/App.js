4132ffbfec50a8f950365284601ab8752afedab4
hyarhyar <arttu.hyvonen@helsinki.fi>
2022-06-18 14:57:42 +0300

    finishing part 1

--- osa1/anecdotes/src/App.js
+++ osa1/anecdotes/src/App.js
@@ -1,12 +1,70 @@
 
 
+import { useState } from 'react'
 
-function App() {
-	return (
-		<div>
-			asdf
-		</div>
-	)
+const Header = ({ text }) => {
+    return (
+        <>
+        <h1>{text}</h1>
+        </>
+    )
 }
 
-export default App;
+const Button = ({text, handleClick}) => {
+    return (
+        <>
+        <button onClick={handleClick}>
+            {text}
+        </button>
+        </>
+    )
+}
+
+
+const App = () => {
+  	const anecdotes = [
+    	'If it hurts, do it more often',
+    	'Adding manpower to a late software project makes it later!',
+    	'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    	'Premature optimization is the root of all evil.',
+    	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    	'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  	]
+
+    const [selected, setSelected] = useState(0)
+  	const [points, setPoints] = useState(anecdotes.map(x => 0))
+
+
+	const incPoints = () => {
+		const tmp = [...points]
+		tmp[selected] += 1
+		return tmp
+	}
+	
+	const maxVotes = () => {
+		return points.indexOf(Math.max(...points))
+	}
+
+
+	return (
+    	<div>
+		<Header text={"Anecdote of the day"}/>
+      	{anecdotes[selected]}
+	  	<br />
+		has {points[selected]} votes
+		<br />
+	  	<Button text={"vote"} handleClick={() => {
+        	setPoints(incPoints)}} />
+    	<Button text={"next anecdote"} handleClick={() => {
+        	setSelected(Math.floor(Math.random()*anecdotes.length))}} />
+    	<Header text={"Anecdote with most votes"}/>
+		{anecdotes[maxVotes()]}
+	  	<br />
+		has {points[maxVotes()]} votes
+		<br />
+		</div>
+  )
+}
+
+export default App
